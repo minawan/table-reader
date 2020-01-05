fe99f11e2161bfdd767cf7bd47f5d1d4201a944b
Minaki Seirawan <Seirawan.Minaki@gmail.com>
2020-01-04 20:54:43 -0800

    Generate column literals

--- src/TableReader.hs
+++ src/TableReader.hs
@@ -8,6 +8,7 @@ module TableReader
   ) where
 
 import Data.Aeson
+import qualified Data.Char as Char
 import qualified Data.List as List
 import qualified Data.Text as Text
 import Data.Text (Text)
@@ -52,14 +53,59 @@ getParseNamedRecordInApplicativeForm columns varName =
     project component = Text.concat [ varName, " .: \"", component, "\""]
     names = map columnName columns
 
+produceTableLiterals :: Text -> [Text] -> String
+produceTableLiterals tableName columnNames =
+    unlines
+      [ firstLetterToLowercase $ printf "%sLiterals :: [String]" tableName
+      , firstLetterToLowercase $ printf "%sLiterals =" tableName
+      , printf "    [ %s" $ List.intercalate "\n    , "
+          . map formatName
+          $ tableName : columnNames
+      , "    ]"
+      ]
+  where
+    firstLetterToLowercase "" = ""
+    firstLetterToLowercase (x:xs) = Char.toLower x : xs
+    formatName x = printf "\"%s\"" x
+
+produceColumnLiterals :: Text -> [Text] -> String
+produceColumnLiterals tableName columnNames =
+    unlines $
+      [ printf "produceStringLiteral :: %s -> String" tableName
+      , printf "produceStringLiteral x ="
+      , printf "  unlines"
+      , printf "    [ %s" . List.intercalate "    , \"\"\n    , " $ map callFormatColumn columnNames
+      , printf "    ]"
+      , printf "  where"
+      ] ++ map formatColumn columnNames
+  where
+    callFormatColumn colName = printf "format%s x" colName
+    formatColumn colName = printf "    format%s field = firstLetterToLowercase (%s field) ++ \"Literal :: String\\n\" ++ firstLetterToLowercase (%s field) ++ \"Literal = \\\"\" ++ %s field ++ \"\\\"\"" colName colName colName colName
+
+isStringColumn :: Column -> Bool
+isStringColumn (Column _ columnType) = columnType == "String" 
+
+produceFirstLetterToLowercase :: String
+produceFirstLetterToLowercase =
+    unlines
+      [ "firstLetterToLowercase :: String -> String"
+      , "firstLetterToLowercase \"\" = \"\""
+      , "firstLetterToLowercase (x:xs) = Char.toLower x : xs"
+      ]
+
 convertToHaskellLib :: Table -> Text
 convertToHaskellLib (Table tableName tableColumns _) =
     Text.pack . unlines $
       [ "{-# LANGUAGE OverloadedStrings #-}"
       , "module ScreenElement where"
       , ""
+      , "import qualified Data.Char as Char"
       , "import Data.Csv"
       , ""
+      , produceFirstLetterToLowercase
+      , ""
+      , produceTableLiterals tableName columnNames
+      , produceColumnLiterals tableName [columnName column | column <- tableColumns, isStringColumn column]
       , printf "data %s = %s" tableName tableName
       , printf "  { %s" (convertColumnsToHaskell tableColumns)
       , "  } deriving (Show)"
@@ -68,12 +114,14 @@ convertToHaskellLib (Table tableName tableColumns _) =
       , printf "  parseNamedRecord record = %s <$> %s" tableName
           (getParseNamedRecordInApplicativeForm tableColumns "record")
       ]
+  where columnNames = map columnName tableColumns
 
 convertToHaskellBin :: Table -> Text
 convertToHaskellBin (Table tableName _ _) =
     Text.pack . unlines $
       [ "import Data.Csv (Header, decodeByName)"
       , "import qualified Data.ByteString.Lazy as B"
+      , "import qualified Data.Vector as Vector"
       , "import Data.Vector (Vector)"
       , "import System.Environment"
       , ""
@@ -86,5 +134,7 @@ convertToHaskellBin (Table tableName _ _) =
       , printf "  let decodedCsvData = decodeByName csvData :: Either String (Header, Vector %s)" tableName
       , "  case decodedCsvData of"
       , "    Left err -> putStrLn err"
-      , "    Right (_, val) -> putStrLn $ show val"
+      , "    Right (_, val) -> do"
+      , "      putStrLn $ show val"
+      , "      putStrLn . unlines . map produceStringLiteral $ Vector.toList val"
       ]
