7e1fb6fc3651418d6a87a5998bacab98c33afa6b
Minaki Seirawan <Seirawan.Minaki@gmail.com>
2020-01-04 17:07:21 -0800

    Migrate TableReader to Stack

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,28 @@
 module Main where
 
-import Lib
+import Data.Aeson
+import qualified Data.ByteString.Lazy as B
+import Data.Text.IO as I hiding (putStrLn)
+
+import TableReader
+
+jsonFile :: FilePath
+jsonFile = "ScreenElement.json"
+
+outputLibFile :: FilePath
+outputLibFile = "ScreenElement.hs"
+
+outputBinFile :: FilePath
+outputBinFile = "GenerateScreenElement.hs"
+
+getRawJSON :: FilePath -> IO B.ByteString
+getRawJSON = B.readFile
 
 main :: IO ()
-main = someFunc
+main = do
+  res <- (eitherDecode <$> getRawJSON jsonFile) :: IO (Either String Table)
+  case res of
+    Left err -> putStrLn err
+    Right table -> do
+      I.writeFile outputLibFile $ convertToHaskellLib table
+      I.writeFile outputBinFile $ convertToHaskellBin table
---
+++ src/TableReader.hs
@@ -0,0 +1,88 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module TableReader
+  ( Table
+  , convertToHaskellLib
+  , convertToHaskellBin
+  ) where
+
+import Data.Aeson
+import qualified Data.List as List
+import qualified Data.Text as Text
+import Data.Text (Text)
+import GHC.Generics
+import Text.Printf (printf)
+
+data Table =
+  Table { tableName :: !Text
+        , tableColumns :: ![Column]
+        , tableLevels :: ![Level]
+        } deriving (Generic, Show)
+
+data Column =
+  Column { columnName :: !Text
+         , columnType :: !Text
+         } deriving (Generic, Show)
+
+data Level =
+  Level { levelName :: !Text
+        , sublevels :: ![Text]
+        } deriving (Generic, Show)
+
+instance FromJSON Table
+instance FromJSON Column
+instance FromJSON Level
+
+convertColumnsToHaskell :: [Column] -> Text
+convertColumnsToHaskell columns =
+    Text.concat . List.intersperse separator $ map declare columns
+  where
+    separator = "\n  , "
+    declare (Column columnName columnType) =
+      Text.concat [columnName, " :: !", columnType]
+
+-- Convert varName and a list [(columnName = name_i, columnType = _)] to:
+--   varName .: "name_1" [<*> varName .: "name_i"]*
+getParseNamedRecordInApplicativeForm :: [Column] -> Text -> Text
+getParseNamedRecordInApplicativeForm columns varName =
+    Text.concat . List.intersperse separator $ map project names
+  where
+    separator = " <*> "
+    project component = Text.concat [ varName, " .: \"", component, "\""]
+    names = map columnName columns
+
+convertToHaskellLib :: Table -> Text
+convertToHaskellLib (Table tableName tableColumns _) =
+    Text.pack . unlines $
+      [ "{-# LANGUAGE OverloadedStrings #-}"
+      , "module ScreenElement where"
+      , ""
+      , "import Data.Csv"
+      , ""
+      , printf "data %s = %s" tableName tableName
+      , printf "  { %s" (convertColumnsToHaskell tableColumns)
+      , "  } deriving (Show)"
+      , ""
+      , printf "instance FromNamedRecord %s where" tableName
+      , printf "  parseNamedRecord record = %s <$> %s" tableName
+          (getParseNamedRecordInApplicativeForm tableColumns "record")
+      ]
+
+convertToHaskellBin :: Table -> Text
+convertToHaskellBin (Table tableName tableColumns _) =
+    Text.pack . unlines $
+      [ "import Data.Csv (Header, decodeByName)"
+      , "import qualified Data.ByteString.Lazy as B"
+      , "import Data.Vector (Vector)"
+      , ""
+      , "import ScreenElement"
+      , ""
+      , "main :: IO ()"
+      , "main = do"
+      , printf "  csvData <- B.readFile \"ScreenElement.csv\""
+      , printf "  let decodedCsvData = decodeByName csvData :: Either String (Header, Vector %s)" tableName
+      , "  case decodedCsvData of"
+      , "    Left err -> putStrLn err"
+      , "    Right (_, val) -> putStrLn $ show val"
+      ]
